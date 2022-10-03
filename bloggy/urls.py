65fa87d73675c975bbdff4863cf34df0724ab503
Vin <maharjanebik061@gmail.com>
2022-10-03 08:27:44 +0545

    Chat removed

--- bloggy/urls.py
+++ bloggy/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('we_blog.urls')),
     path('we_blog/', include('we_blog.urls'))
 ]
